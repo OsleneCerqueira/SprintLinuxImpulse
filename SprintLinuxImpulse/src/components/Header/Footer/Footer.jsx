abe8c6bc84e38892c544d5076f9a062209b7b853
OsleneCerqueira <oslene.cerqueira@gmail.com>
2022-09-10 19:08:49 -0300

    ✨ feat: Component Footer

---
+++ SprintLinuxImpulse/src/components/Header/Footer/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { FooterStyle } from './styledFooter';
+
+function Footer() {
+  return (
+    <FooterStyle>
+      <p>
+        Testando suas habilidades em HTML, CSS e JS.
+
+      </p>
+      <p> Linx Impulse</p>
+      <p>2019</p>
+    </FooterStyle>
+  );
+}
+
+export default Footer;
