ceb7697132cbd91ce864f7aaf6ec696ef9560f5c
OsleneCerqueira <oslene.cerqueira@gmail.com>
2022-09-10 21:53:13 -0300

    ✨ feat: Styled Componet Produto e Separador

---
+++ SprintLinuxImpulse/src/components/Main/Produto/styledProduto.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+export const ProdutosStyle = styled.section`
+  width: 100%;
+  display: flex;
+  margin-left:3rem;
+  align-items: center;
+  gap: 3rem;
+  flex-wrap: wrap;
+`;
+
+export const Separador = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 1.5rem;
+  margin-top: 5rem;
+hr{
+    width: 400px;
+    height: 1px;
+    background-color: #707070;
+  }
+`;
