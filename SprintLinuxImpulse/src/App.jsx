97c5cc2c3612cf07c7b4553df85b71fb7e49f68d
OsleneCerqueira <oslene.cerqueira@gmail.com>
2022-09-10 21:58:52 -0300

    ✨ feat: Componet App

--- SprintLinuxImpulse/src/App.jsx
+++ SprintLinuxImpulse/src/App.jsx
@@ -1,10 +1,14 @@
-import './App.css';
+import Header from './components/Header/Header';
+import Main from './components/Main/Main';
+import Footer from './components/Footer/Footer';
 
 function App() {
-
   return (
-
+    <section>
+      <Header />
+      <Main />
+      <Footer />
+    </section>
   );
 }
-
 export default App;
