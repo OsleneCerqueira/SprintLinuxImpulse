dcbe52bd0b9e9834376f19fe88f9095a49d68f2f
OsleneCerqueira <oslene.cerqueira@gmail.com>
2022-09-10 21:36:07 -0300

    ✨ feat: Styled Component Input e Label

---
+++ SprintLinuxImpulse/src/components/Input/Input.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const Input = styled.input`
+  width:340px;
+  height: 30px;
+  background: #FFFFFF 0% 0% no-repeat padding-box;
+  border: 1px solid #707070;
+  padding-left: 10px;
+`;
+export const Label = styled.label`
+  font-size: 0.8rem;
+  text-align: left;
+  letter-spacing: 0px;
+  color: #888888;
+  opacity: 1;
+`;
