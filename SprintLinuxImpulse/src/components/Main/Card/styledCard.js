7d714faf5db24f7aa31ddc9ddc2c111785391259
OsleneCerqueira <oslene.cerqueira@gmail.com>
2022-09-10 21:49:19 -0300

    ✨ feat: Styled Componets Card

---
+++ SprintLinuxImpulse/src/components/Main/Card/styledCard.js
@@ -0,0 +1,37 @@
+import styled from 'styled-components';
+
+export const CardStyle = styled.section`
+  width:20%;
+  align-items: start;
+  margin-top: 1.5rem;
+section{
+    width: 100%;
+    height: 130px;
+
+    img{
+    width: 100%;
+    height: 100%;
+  }
+  }
+
+div{
+    display: flex;
+    flex-direction: column;
+    gap: 0.4rem;
+  }
+h2{
+    font-size: 14px;
+    text-align: left;
+    color: #888888;
+  }
+p{
+    font-size: 12px;
+    text-align: left;
+    color: #888888;
+  }
+h4{
+    font-size: 16px;
+    font-weight: bold;
+    text-align: left;
+    color: #888888;
+    }`;
